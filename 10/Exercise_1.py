d4a387dc5b8a9a6738658893a55b6f69ff18805a
Matthew Kenny <matthewkenny@gmail.com>
2012-11-01 19:35:58 -0700

    Finished recursive example for Exercise 10.1.

--- 10/Exercise_1.py
+++ 10/Exercise_1.py
@@ -17,13 +17,25 @@ def nested_sumConditional(listin):
     """Use conditional to return sum of all elements"""
     total = 0
     for innerlist in listin:
-        if isinstance(listin, list):
+        if isinstance(innerlist, list):
             for element in innerlist:
                 total += element
         else:
             total += innerlist
     return total
 
+def nested_sumWhileRecursive(listin, total=0):
+    """Use while loop and recursion to return sum of all elements"""
+    counter = len(listin)
+    while counter > 0:
+        if isinstance(listin[0], list):
+            for element in listin[0]:
+                total += element
+        else:
+            total += listin[0]
+        del listin[0]
+        return nested_sumWhileRecursive(listin, total)
+    return total
 
 if __name__ == '__main__':
     print "Run One."
@@ -32,3 +44,6 @@ if __name__ == '__main__':
     print "Run Two."
     twoList = [[3,3],[3,3],[3,3],3,3]
     print nested_sumConditional(twoList)
+    print "Run Three."
+    threeList = [[3,3],[3,3],[3,3],3,3]
+    print nested_sumWhileRecursive(threeList)
