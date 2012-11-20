bb14f61a8561d642088a4618bc2f62f89e4c77d6
Matthew Kenny <matthewkenny@gmail.com>
2012-11-19 18:01:18 -0800

    Thread sharing example.

--- Working_Junk/thread_example.py
+++ Working_Junk/thread_example.py
@@ -6,16 +6,31 @@ Insert those into a queue
 Have a conumer grab products from the queue and perform work.
 """
 
+import random, time
+
 def producer_ready():
     """Return true to kick off the work"""
-    while val
-    val = raw_input("Ready >>>")
-    return val
+    val = 'stop'
+    while val <> 'go':
+        val = raw_input("Ready >>>")
+    return True
+
+def producer():
+    """Return a random number every set interval of time."""
+    for i in range(100):
+        time.sleep(1)
+        return random.random
+
+def consumer(a):
+    """return a square"""
+    return a**2
 
 if __name__ == '__main__':
+    q = []
+
     while True:
         if producer_ready():
             product = producer()
             q.insert (0,product)
         if len(q) > 0:
-            consumer(q.pop())
+            print consumer(q.pop())
