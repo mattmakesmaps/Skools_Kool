7b924a3d83315e9f661c9d69a7e92a133a947f78
Matthew Kenny <matthewkenny@gmail.com>
2012-11-24 13:40:02 -0800

    Exercises Chapter 17 1-5

--- 15/Exercise_1.py
+++ 15/Exercise_1.py
@@ -11,6 +11,24 @@ class Point(object):
     def coord(self):
         return [self.x,self.y]
 
+    def __str__(self):
+        return 'Point: %s, %s' % (self.x, self.y)
+
+    def __add__(self, other): # It's convention to use self, other. Other is another instance.
+        """Return sum of two coordinates.
+           An example of operator overloading."""
+        if isinstance(other, Point): # Assume a Point
+            sumX = self.x + other.x
+            sumY = self.y + other.y
+        else: # Assume a tuple
+            sumX = self.x + other[0]
+            sumY = self.y + other[1]
+        return Point(sumX, sumY)
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+
 def distance_between_points(p1, p2):
     """Return distance between two points"""
     try:
@@ -22,5 +40,17 @@ def distance_between_points(p1, p2):
 if __name__ == '__main__':
     myP1 = Point(10,99.9999)
     myP2 = Point(10,15)
+    myTuple = (22,22)
 
     print distance_between_points(myP1,myP2)
+    print myP2
+    # Operator Overload
+    print myP1 + myP2
+    print type(myP1 + myP2)
+    # Use __add__
+    print myP1 + myTuple
+    print type(myP1 + myTuple)
+    # Use __radd__
+    print myTuple + myP1
+    print type(myTuple + myP1)
+
---
+++ 17/Exercise_Listing.py
@@ -0,0 +1,39 @@
+__author__ = 'matt'
+
+"""
+Exercise 1:
+Rewrite time_to_int (from Section 16.4) as a method. It is probably not appropriate to rewrite int_to_time as a method; what object you would invoke it on?
+
+I attempted this already in Exercise_1_2_3.py, through the methods time.to_hms(), time.to_secods().
+"""
+
+"""
+Exercise 2:
+Write an init method for the Point class that takes x and y as optional parameters and assigns them to the corresponding attributes.
+
+Done. See Chapter 15 Exercise 1
+"""
+
+"""
+Exercise 3:
+Write a str method for the Point class. Create a Point object and print it.
+
+Done. See Chapter 15 Exercise 1
+"""
+
+
+"""
+Exercise 4:
+Write an add method for the Point class.
+
+Done. See Chapter 15 Exercise 1.
+"""
+
+"""
+Exercise 5:
+Write an add method for Points that works with either a Point object or a tuple:
+If the second operand is a Point, the method should return a new Point whose x coordinate is the sum of the x coordinates of the operands, and likewise for the y coordinates.
+If the second operand is a tuple, the method should add the first element of the tuple to the x coordinate and the second element to the y coordinate, and return a new Point with the result.
+
+Done. See Chapter 15 Exercise 1.
+"""
